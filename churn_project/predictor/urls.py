c05437fc3e0dd8853f93024ff37e9e5ca8dd62d0
shahidkx07 <shahidkhan88892@gmail.com>
2026-06-24 22:21:42 +0530

    ui fix and route redirecting

--- churn_project/predictor/urls.py
+++ churn_project/predictor/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name="dashboard", permanent=False)),
     path("predict/", views.predict, name="predict"),
     path("dashboard/", views.dashboard, name="dashboard"),
 ]
